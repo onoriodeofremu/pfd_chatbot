5544111274ee7f025b17fa7a8ae0a4269bba18f7
Ofremu199x <Ofremu199x@gmail.com>
2026-09-11 12:44:09 +0100

    made some changes

--- pdf_app.py
+++ pdf_app.py
@@ -10,7 +10,7 @@ import tempfile
 
 import streamlit as st
 
-import Practice.ai_prompts.pdf.pdfbot as pdfbot
+import pdfbot
 from Practice.ai_prompts.pdf.pdfbot import PDFBot
 
 st.set_page_config(page_title="PDF Q&A", page_icon="📄", layout="centered")
